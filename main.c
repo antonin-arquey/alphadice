ee5d7ba6ea5a9a318b1350486c9782bb805c80db
Julien TRIAU <julien.triau@etu.univ-nantes.fr>
2017-03-13 18:54:30 +0100

    Ajout de commentaires dans le main

--- main.c
+++ main.c
@@ -7,21 +7,30 @@
 #include "interface.h"
 
 int main(int argc, char* argv[]){
+	/* Récupération des paramètres */
 	int nbPlayer, nbGame;
 	if(verifArguments(argc, argv)){
 		return 1;
 	}
 	nbPlayer = *argv[2];
 	nbGame = *argv[1];
+	
+	/* Création de l'affichage*/ 
 	SDL_Window* window = createWindow();
 	SDL_Renderer* renderer = createRenderer(window);
 	SDL_RenderPresent(renderer);
 	displayMap(renderer);
 	SDL_RenderPresent(renderer);
+	
+	/*Initialisation du jeu */
 	//InitGame(unsigned int id, nbPlayer, SPlayerInfo *info); //id de quel joueur ? *info de quel joueur ?
+	
+	/* Boucle du jeu (doit se terminer lorsque l'on ferme la fenêtre ou que l'on quitte proprement le jeu) */
 	while(windowIsNotClosed()){
 
 	}
+	
+	/* Ferme le jeu */
 	destroyWindow(window, renderer);
 	return 0;
 }

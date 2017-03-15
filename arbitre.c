043b20443e929364accf00101c46bdf3007fef21
Julien TRIAU <julien.triau@etu.univ-nantes.fr>
2017-03-15 12:46:51 +0100

    Modification de la fonction getDiceToDistribute

--- arbitre.c
+++ arbitre.c
@@ -60,6 +60,32 @@ int getDiceToDistribute(int idPlayer, SMap *map){
 	/*colorier x avec i
 	pour tout sommet y successeur de x
 	  si y n'est pas colorié faire CC_sommet(G,y,i)*/
+	  
+	// SCell* tabCell[map->nbCells];
+	// SCell* tabVoisins[map->nbCells];
+	// int i = 0, y = 0, j = 0;
+	// SCell* cellActive;
+	// for(;i<nbCells;i++){
+		// if (map->cells[i]->owner == idPlayer){
+			// tabCell[y]=map->cells[i];
+			// y++;
+		// }
+	// }
+	// for(i = 0; i<y; i++){
+		// if(tabCell[i]!=NULL){
+			// cellActive = tabCell[i];
+			// for(;j<cellActive->nbNeighbors;j++){
+				// if(cellActive->neighbors[j]->owner==idPlayer){
+					// tabCell = 
+				// }
+			// }
+			// //vérifier ses voisins
+			// //Remplir un tableau des voisins du même joueur
+			// //Retirer ces voisins de tabCell
+			// //Regarder les voisins de chaque voisins
+			// //a chaque récursion augmenter de 1 et retirer les cellules de la liste
+		// }
+	// }
 
 	int nbDices = 0;
 	for(int i = 0; i < map->nbCells; i++){

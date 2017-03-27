a38bf119abe69c6e75859ddfde3cdfa76653da9f
Laouenan LE CORGUILLE <=>
2017-03-27 21:54:53 +0200

    Débuggage de la fonction init des matrices

--- trainIA/main.c
+++ trainIA/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double random();
+double** init_rand(int wx, int wy);
+double** init0(int wx, int wy);
+void affichage(double **tab, int ax, int ay);
+
 double random(){
     return 2 * (rand() / (RAND_MAX + 1.)) - 1;
 }
 
-void init_rand(double **w, int wx, int wy){
-  w = malloc(sizeof(double * wy));
+double** init_rand(int wx, int wy){
+  double** w = malloc(sizeof(double)*wy);
   for(int i = 0;i<wy;i++){
-    w[i] = malloc(sizeof(double * wx));
+    w[i] = malloc(sizeof(double)* wx);
   }
 
   for (int i = 0; i < wy; i++) {
@@ -16,12 +21,14 @@ void init_rand(double **w, int wx, int wy){
       w[i][j] = random();
     }
   }
+
+  return w;
 }
 
-void init0(double **w, int wx, int wy){
-  w = malloc(sizeof(double * wy));
+double** init0(int wx, int wy){
+  double** w = malloc(sizeof(double)*wy);
   for(int i = 0;i<wy;i++){
-    w[i] = malloc(sizeof(double * wx));
+    w[i] = malloc(sizeof(double)* wx);
   }
 
   for (int i = 0; i < wy; i++) {
@@ -29,8 +36,9 @@ void init0(double **w, int wx, int wy){
       w[i][j] = 0;
     }
   }
+  return w;
 }
-
+/*
 void recupXY(double X[][3], double Y[][1], double data[][4], int debut, int fin){
   for (int i = debut; i < fin; i++) {
     Y[i][1] = data[i][4];
@@ -38,18 +46,19 @@ void recupXY(double X[][3], double Y[][1], double data[][4], int debut, int fin)
       X[i][h] = data[i][h];
     }
   }
-}
+}*/
 
 void affichage(double **tab, int ax, int ay){
   printf("--affichage du tableau --\n");
   for(int i = 0; i < ay; i++){
     for(int j = 0; j < ax; j++){
-      printf("%f ", tab[i][j]);
+      printf("%f \t", tab[i][j]);
     }
     printf("\n");
   }
   printf("-- fin d'affichage --\n");
 }
+/*
 void dot(double **out, double **a, int ax, int ay, double **b, int bx, int by){
   init0(out,bx,ay);
   double val=0;
@@ -71,10 +80,15 @@ void transpose(double outT[1][5], double out[5][1]){
     for (int i = 0; i < 5; i++) {
       outT[0][i] = out[i][0];
     }
-}
+}*/
 
 int main(int argc, char const *argv[]) {
-  double data[5][4] = {{0,1,3,0},
+  double **m;
+  int mx=3;
+  int my=5;
+  m = init_rand(mx,my);
+  affichage(m,mx,my);
+  /*double data[5][4] = {{0,1,3,0},
                      {3,1,0,1},
                      {0,1,0,1},
                      {-2,0,2,0},
@@ -103,6 +117,6 @@ int main(int argc, char const *argv[]) {
     transpose(outT, out);
     dot(update, outT, err, 5);//pas err normalement mais err_delta
     add(weights,update);
-  }
+  }*/
   return 0;
 }

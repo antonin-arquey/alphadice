22a6a97e17192f0302568e30c96e19c1f73f738b
Julien TRIAU <julien.triau@etu.univ-nantes.fr>
2017-03-29 22:14:51 +0200

    Parser en attente de modification du log pour amélioration

--- Parser/parser.c
+++ Parser/parser.c
@@ -5,6 +5,7 @@ int main (int argc,char* argv[]){
 	FILE *fp;
 	char buff[255];
 	int i, nbTer, etat = 0;
+	char caract;
 	
 	fp = fopen("game.log", "r");
 	fscanf(fp, "%s", buff);
@@ -19,20 +20,46 @@ int main (int argc,char* argv[]){
 	}
 	
 	do{
-		lseek(fp, -1, SEEK_CUR);
+		fseek(fp, -1, SEEK_CUR);
+		fgets(buff, 255, (FILE*)fp); //Prends une ligne
+		strtok(buff, "\n");
+		//printf("%s\n",buff );
+		if((strncmp(buff,"/-/",3))==0){
+			etat=4;
+		}else if((strncmp(buff,"///",3))==0){
+			etat=4;
+		}else if((strncmp(buff,"###",3))==0){
+			etat=4;
+		}
+		
 		switch(etat){
 			//case 0 : case nothing
 			case 0 : 
-				lseek(fp, +1, SEEK_CUR);
+				//lseek(fp, +1, SEEK_CUR);
 				etat=1;
 				break;
 			//case 1 : case tour
 			case 1 : 
-				//fscanf(fp, "%s", buff); //Prends un caractere
-				fgets(buff, 255, (FILE*)fp); //Prends une ligne
-				printf("1 : %s\n", buff );
+				printf("YOLO à nous le jeux\n");
 				break;
+			// Fin d'un tour
+			case 2 :
+				printf("FIN d'un tour\n");
+				printf("Le terrain n°%s reçoit un dé\n", buff);
+				break;
+			case 3 :
+				printf("The Winner is\n");
+				break;
+			case 4 :
+				if((strncmp(buff,"/-/",3))==0){
+					etat=1;
+				}else if((strncmp(buff,"///",3))==0){
+					etat=2;
+				}else if((strncmp(buff,"###",3))==0){
+					etat=3;
+				}
 		}
-	}while(fgetc(fp) != EOF);
+		caract = fgetc(fp);
+	}while(caract != EOF);
 	
 }

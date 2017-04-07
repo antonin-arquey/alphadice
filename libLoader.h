b4baa9e5f79b6bd4a4739ff8ce033805d3305242
Julien TRIAU <julien.triau@etu.univ-nantes.fr>
2017-04-07 09:47:16 +0200

    Ajout de commentaire et modification de l'indentation

--- libLoader.h
+++ libLoader.h
@@ -3,11 +3,19 @@
 
 #include "interface.h"
 
+//
 typedef int (*playT)(int, const SMap*, STurn*);
+
+//
 typedef void (*initG)(unsigned int, unsigned int, SPlayerInfo*);
+
+//
 typedef void (*endG)(unsigned int, unsigned int);
 
+//
 void** loadLib(int nbLib, char *argv[], initG initTab[], playT playTab[], endG endTab[]);
+
+//
 void freeLib(void **libs, int nbLib);
 
 #endif
--- main.c
+++ main.c
@@ -10,12 +10,15 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
-	/* Récupération des paramètres */
+	/* Déclaration des variables */
 	int nbPlayer, nbGame;
 	int nbLib = 0;
 	int matrice_map[WIN_WIDTH][WIN_HEIGHT];
 	int tab_pays[80][2];
+	SMap *map = NULL;
+	STurn *turn = malloc(sizeof(STurn));
 
+	//Vérifie les arguments et les confirme.
 	if(verifArguments(argc, argv, &nbLib)){
 		return 1;
 	}
@@ -29,7 +32,7 @@ int main(int argc, char* argv[]){
 	initG InitGame[nbLib];
 	endG EndGame[nbLib];
 
-
+	//On vérifie si il y a des librairies à charger et on les charge.
 	if(nbLib > 0) //Si au moins une lib dynamique a charger
 	{
 		libs = loadLib(nbLib, argv, InitGame, PlayTurn, EndGame);
@@ -46,19 +49,15 @@ int main(int argc, char* argv[]){
 	/* Création de l'affichage*/
 	SDL_Window* window = createWindow();
 	SDL_Renderer* renderer = createRenderer(window);
-
 	SDL_Texture *diceTextures[65];
 	loadDiceTextures(renderer, diceTextures);
-
 	SDL_Texture *scoreTextures[10];
 	loadScoreTextures(renderer, scoreTextures);
 
-	SMap *map = NULL;
-
-	STurn *turn = malloc(sizeof(STurn));
 	// Boucle du jeu (doit se terminer lorsque l'on ferme la fenêtre ou que l'on quitte proprement le jeu)
 	for(int p=0 ; p < nbGame ; p++)
 	{
+		//Début de chaque partie
 		int vic = 0;
 		printf("GAME %d / %d\n", p+1, nbGame);
 		map = createMap(nbPlayer, renderer, matrice_map, tab_pays);
@@ -67,6 +66,7 @@ int main(int argc, char* argv[]){
 		SDL_RenderPresent(renderer);
 
 		while(windowIsNotClosed() && !vic){
+			//Début de chaque tour
 			for(int i = 0; i < nbPlayer; i++){
 				//Tour des joueurs humains
 				if(i+nbLib < nbPlayer){
@@ -96,14 +96,14 @@ int main(int argc, char* argv[]){
 					freeMap(mapCopy);
 				}
 
+				//Si le joueur a gagné
 				if(victoire(i, map)){
 					char str[20];
 					sprintf(str, "###\n%d\n###\n",  i);
 					Log(str);
 					printf("Victoire du joueur %d !!\n", i);
 					vic = 1;
-				}
-				else{
+				}else{ //Sinon
 					endTurn(i, map);
 				}
 				displayMap(renderer,map,matrice_map,turn, tab_pays, diceTextures,  scoreTextures, i);
@@ -112,7 +112,7 @@ int main(int argc, char* argv[]){
 		}
 	}
 
-
+	//On libère toutes les variables
 	freeDiceTextures(diceTextures);
 	freeScoreTextures(scoreTextures);
 	free(turn);
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]){
 	}
 	free(info);
 
-	// Ferme le jeu
+	//On ferme le jeu
 	destroyWindow(window, renderer);
 	freeLib(libs, nbLib);
 	return 0;
